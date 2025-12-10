2ff1919559ef1fea87ddc5f5279cc380b7ff9336
L0NELYB0Y <dogemaga123@gmail.com>
2025-12-10 12:41:32 +0000

    Comitt Atividade 2 V1

--- Parte2/script.js
+++ Parte2/script.js
@@ -7,10 +7,13 @@ function carregar(){
     msg.innerHTML = `Agora sao ${hora} horas.`
     if (hora >= 0 && hora < 12){
         img.src='img/manha.jpg'
+        document.body.style.background='#e2cd9f'
     } else if (hora >= 12 && hora <=18){
         img.src='img/tarde.jpg'
+        document.body.style.background='#b9846f'
     } else{
         img.src='img/noite.jpg'
+        document.body.style.background='#515154'
     }
 
 
